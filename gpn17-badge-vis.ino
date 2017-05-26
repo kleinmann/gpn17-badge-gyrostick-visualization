32881c24e8f1a40d38d7dda65cd4c75bc98b7740
Uwe Kleinmann <uwe@kleinmann.org>
2017-05-26 22:41:34 +0200

    Use gyrostick library.

--- gpn17-badge-vis.ino
+++ gpn17-badge-vis.ino
@@ -2,36 +2,34 @@
 #include <FS.h>
 #include <BadgeUI.h>
 #include <UIThemes.h>
-#include <math.h>
 
 #include "rboot.h"
 #include "rboot-api.h"
 
-#include "sensor.h"
+#include <gyrostick.h>
 
 Badge badge;
-
-imu::Vector<3> data;
+Gyrostick gyrostick;
 
 #define COLOR_WHITE 0xFFFF
 #define COLOR_BLACK 0x0000
 
 void setup() {
   badge.init();
+  bno.begin();
 
   SPIFFS.begin();
 
   tft.fillScreen(COLOR_BLACK);
   tft.writeFramebuffer();
   badge.setBacklight(true);
-  
+
   // TODO: Is this needed?
   badge.setGPIO(IR_EN, LOW);
   badge.setAnalogMUX(MUX_JOY);
 
-  bno.begin();
   delay(300);
-  badge.setGPIO(MQ3_EN,1);
+  badge.setGPIO(MQ3_EN, 1);
 
   rboot_config rboot_config = rboot_get_config();
   File f = SPIFFS.open("/rom" + String(rboot_config.current_rom), "w");
@@ -40,45 +38,21 @@ void setup() {
 
 void loop() {
   tft.fillScreen(COLOR_BLACK);
-  
-  data = euler();
 
-  double x = getUsableX(data.x());
-  double y = constrain(data.y() * 8.0, -180.0, 180.0) + 180.0;
-  double z = constrain((data.z() - 60.0) * 8.0, -180.0, 180.0) + 180.0; // holding device in front of you means "centered"
+  imu::Vector<2> coords = gyrostick.coords();
 
-  tft.setCursor(10, 60);
-  tft.println(x);
-  tft.setCursor(10, 80);
-  tft.println(y);
-  tft.setCursor(10, 100);
-  tft.println(z);
+  tft.setCursor(2, 2);
+  tft.println(coords.x());
+  tft.setCursor(2, 20);
+  tft.println(coords.y());
 
   tft.drawCircle(
-    (y / 360.0) * 128.0, // x-coord
-    (z / 360.0) * 128.0, // y-coord
-    5.0, //(x / 360.0) * 32.0, // scale
+    coords.x(),
+    coords.y(),
+    5.0, // scale
     COLOR_WHITE);
 
   tft.writeFramebuffer();
-  
+
   pixels.show();
 }
-
-double getUsableX(double x) {
-  if(x >= 180.0) {
-    // x geht nach rechts
-    // x geht von 360 nach 180 (negativ)
-    // 360 = 1, 359 = 2, 358 = 3)
-    x = (x - 360) * (-1);
-  } else { // x < 180.0)
-    // x geht nach links
-    // x geht von 0 nach 180 (positiv)
-    x = x * (-1);
-  }
-
-  x = x + 180.0;
-
-  return x;
-}
-
